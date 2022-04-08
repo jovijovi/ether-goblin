368496e4ea084344d2abceeecf324eab770e4013
jovijovi <mageyul@hotmail.com>
2022-04-08 18:00:36 +0800

    feat(contracts): add mint func

--- contracts/nft/YaNFT.sol
+++ contracts/nft/YaNFT.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // A simple NFT contract
-contract YaNFT is ERC721Enumerable {
+contract YaNFT is ERC721Enumerable, Ownable {
     constructor() ERC721("YaNFT", "YNFT") {
+    }
+
+    function mint(address to, uint256 tokenId) external onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    function mintWithURI(address to, uint256 tokenId, bytes memory uri) external onlyOwner {
+        _safeMint(to, tokenId, uri);
     }
 }
